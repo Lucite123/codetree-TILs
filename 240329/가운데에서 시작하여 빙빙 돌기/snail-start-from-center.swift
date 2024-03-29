3f4093620029ea1c5938f7105683505eb581d1dd
Lucite123 <164841152+Lucite123@users.noreply.github.com>
2024-03-29 15:36:24 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 1046ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

---
+++ 240329/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.swift
@@ -0,0 +1,65 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+var table = [[String]]()
+
+for _ in 0 ..< n {
+    var row = [String]()
+    for _ in 0 ..< n {
+        row.append("0")
+    }
+
+    table.append(row)
+}
+
+func isValidRange(_ x: Int, _ y: Int) -> Bool {
+    return (x >= 0 && x <= n - 1) && (y >= 0 && y <= n - 1)
+}
+
+let dxs = [0, 1, 0, -1]
+let dys = [1, 0, -1, 0]
+
+
+
+var x = (n - 1) / 2
+var y = (n - 1) / 2
+var moveNumber = 0
+var targetMoveNumber = 0
+var dir = 0
+
+var number = 1
+
+while number <= n * n {
+
+    while moveNumber >= 0 {
+        if (number > n * n) {
+            break
+        }
+
+        table[x][y] = String(number)
+
+        if (isValidRange(x + dxs[dir], y + dys[dir]) == false) {
+            break
+        }
+        x += dxs[dir]
+        y += dys[dir]
+        number += 1
+        moveNumber -= 1
+    }
+
+    
+
+    // print(table)
+
+    dir = ((dir - 1) + 4) % 4
+    if dir == 0 || dir == 2 {
+        targetMoveNumber += 1
+    }
+    moveNumber = targetMoveNumber
+}
+
+
+for row in table {
+    print(row.joined(separator: " "))
+}
