44450587c9b218bc5367b052bbe6cc155eece6dd
Lucite123 <164841152+Lucite123@users.noreply.github.com>
2024-04-01 15:09:01 +0900

    Title: 그래프 탐색, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240401/그래프 탐색/graph-traversal.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+let input = readLine()!.components(separatedBy: " ").map { Int(String($0))! }
+let n = input[0]
+let m = input[1]
+
+var graph: [[Int]] = [[0]]
+var visited = Array(repeating: false, count: n + 1)
+
+// 정답: 1번이 스택에서 나오기 전까지 들어간 최대 깊이 수
+
+for _ in 0 ..< n {
+    graph.append([Int]())
+}
+
+for _ in 0 ..< m {
+    let input = readLine()!.components(separatedBy: " ").map { Int(String($0))! }
+    let vertex = input[0]
+    let connectedVertex = input[1]
+    graph[vertex].append(connectedVertex)
+    graph[connectedVertex].append(vertex)
+}
+
+func dfs(_ vertex: Int) {
+    for i in 0 ..< graph[vertex].count {
+        let currentVertex = graph[vertex][i]
+        if visited[currentVertex] == false {
+            visited[currentVertex] = true
+            dfs(currentVertex)
+        }
+    }
+}
+
+dfs(1)
+
+print(visited.filter { $0 == true }.count - 1)
