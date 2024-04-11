239b3772698d789df80c235ae9e111528b113a2f
Lucite123 <164841152+Lucite123@users.noreply.github.com>
2024-04-11 13:21:04 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 182ms, Memory: 2MB, Status: Passed - CodeTree

---
+++ 240411/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let kn = readLine()!.trimmingCharacters(in: .whitespaces).components(separatedBy: " ").map { Int(String($0))! }
+let k = kn[0]
+let digit = kn[1]
+
+var result = [Int]()    
+
+func Print() {
+    let resultString = result.map { String($0) }
+    print(resultString.joined(separator: " "))
+}
+
+func choice(_ n: Int) {
+    if n == digit {
+        Print()
+        return
+    }
+
+
+    for pickedNumber in 1 ... k {
+        result.append(pickedNumber)
+        choice(n + 1)
+        result.removeLast()
+    }
+
+    return
+}
+
+choice(0)
