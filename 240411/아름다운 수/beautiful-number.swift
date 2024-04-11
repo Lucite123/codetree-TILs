4c084f51e5fc46f7b18e2653b5e8ad61572fee72
Lucite123 <164841152+Lucite123@users.noreply.github.com>
2024-04-11 15:26:36 +0900

    Title: 아름다운 수, Time: 366ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240411/아름다운 수/beautiful-number.swift
@@ -0,0 +1,83 @@
+import Foundation
+
+let digit = Int(readLine()!)!
+// let digit = 6
+
+// 동일한 숫자에 대해 연달아 같은 숫자의 묶음이 나오는 것
+
+var numbers = [Int]()
+var count = 0
+
+func check() -> Bool {
+    var numberTable = [Int: Int]()
+
+    if digit == 1 {
+        if numbers[0] == 1 {
+         
+            return true
+        }
+
+        return false
+    }
+
+    var continuousNumber = numbers[0]
+    numberTable[continuousNumber, default: 0] += 1
+
+    for index in 1 ..< numbers.count {
+        let currentNumber = numbers[index]
+
+        if continuousNumber != currentNumber {
+            let count = numberTable[continuousNumber, default: 0]
+            // print(numberTable)
+            // print(count)
+            // print(continuousNumber)
+            // print(currentNumber)
+            // print("outer")
+            if count % continuousNumber != 0 {
+                // print(count)
+                // print(continuousNumber)
+                // print(currentNumber)
+                // print(numbers)
+                // print("false", "1")  
+                return false
+            }
+            continuousNumber = currentNumber
+            numberTable = [Int: Int]()
+            numberTable[continuousNumber, default: 0 ] += 1
+        } else {
+            numberTable[continuousNumber, default: 0] += 1
+        }
+    }
+
+    let count = numberTable[continuousNumber, default: 0]
+    if count % continuousNumber != 0 {
+        // print(numbers)
+        // if numbers == [1, 1, 1] {
+        //     print(numberTable)
+        // }
+        // print("false", "2")
+        return false
+    }
+
+    // print(numbers)
+    // print("true")
+    return true
+}
+
+func findPermutation(_ n: Int) {
+    if n == digit {
+        if check() {
+            count += 1
+        }
+        return
+    }
+
+    for number in 1 ... 4 {
+        numbers.append(number)
+        findPermutation(n + 1)
+        numbers.removeLast()
+    }
+}
+
+findPermutation(0)
+print(count)
