e40ff0978115d8de4a50c9c9b2b12d1a829eb2b4
Lucite123 <164841152+Lucite123@users.noreply.github.com>
2024-04-13 20:29:07 +0900

    Title: 사다리 타기, Time: 74ms, Memory: 4MB, Status: Passed - CodeTree

--- 240413/사다리 타기/ladder-game.swift
+++ 240413/사다리 타기/ladder-game.swift
@@ -6,90 +6,143 @@ let n = nm[0]
 let m = nm[1]
 
 struct Line: Hashable, Comparable {
+	static func < (lhs: Line, rhs: Line) -> Bool {
+		if lhs.b > rhs.b {
+			return true
+		}
+		
+		return false
+	}
+	
 	let a: Int
 	let b: Int
-
-    static func < (lhs: Line, rhs: Line) -> Bool {
-        if lhs.b > rhs.b {
-            return true
-        }
-
-        return false
-    }
+	
+	
 }
+
+// a번째 세로줄과 a + 1번째 세로줄을 연결 => y
+// 위에서 부터 b번째 위치에 가로줄이 그어짐 => x
+
+
+func initialMap(_ lines: [Line]) -> [[Int]] {
+	var map = Array(repeating: Array(repeating: -1, count: n), count: 15)
+	for (index, line) in lines.enumerated() {
+		map[line.b][line.a] = index + 1
+		map[line.b][line.a + 1] = index + 1
+	}
+	
+	return map
+}
+
+// isValidRange
+// 왼쪽, 오른쪽 비교
+
+func checkRight(_ x: Int, _ y: Int, _ map: [[Int]]) -> Bool {
+	guard y + 1 <= n - 1
+	else { return false}
+	
+	let currentPosition = map[x][y]
+	
+	if currentPosition == map[x][y + 1] {
+		return true
+	}
+	
+	return false
+}
+
+func checkLeft(_ x: Int, _ y: Int, _ map: [[Int]]) -> Bool {
+	guard y - 1 >= 0
+	else { return false }
+	
+	let currentPosition = map[x][y]
+	
+	if currentPosition == map[x][y - 1] {
+		return true
+	}
+	
+	return false
+}
+
+func ladderGame(_ map: [[Int]]) -> [Int: Int] {
+	
+	var result = [Int: Int]()
+	// 0 => 오른쪽
+	// 1 => 왼쪽
+	// 2 => 수직
+	let dxs = [1, 1, 1]
+	let dys = [1, -1, 0]
+	
+	for person in 0 ..< n {
+		var position = (0, person)
+		
+		while position.0 <= 14 {
+			if map[position.0][position.1] == -1 {
+				position.0 += 1
+				continue
+			}
+			
+			if checkLeft(position.0, position.1, map
+			) {
+				position.0 += dxs[1]
+				position.1 += dys[1]
+			} else if checkRight(position.0, position.1, map) {
+				position.0 += dxs[0]
+				position.1 += dys[0]
+			}
+		}
+		
+		result[person] = position.1
+	}
+//	print("end🔥🔥🔥🔥🔥🔥🔥🔥")
+	return result
+}
+
 
 var lines = [Line]()
 
 for _ in 0 ..< m {
-    let line = readLine()!.trimmingCharacters(in: .whitespaces).components(separatedBy: " ").map { Int(String($0))! }
-    lines.append(Line(a: line[0] - 1, b: line[1] - 1))
+	let line = readLine()!.trimmingCharacters(in: .whitespaces).components(separatedBy: " ").map { Int(String($0))! }
+	lines.append(Line(a: line[0] - 1, b: line[1] - 1))
 }
 
-
-func initialResult(_ lines: [Line]) -> [Int] {
-
-    var persons = [Int]()
-
-    for person in 0 ..< n {
-        persons.append(person)
-    }
-
-    for line in lines {
-        let temp = persons[line.a]
-        persons[line.a] = persons[line.a + 1]
-        persons[line.a + 1] = temp
-    }
-
-    return persons
-}
 lines = lines.sorted()
-let pivot = initialResult(lines)
-
-var selectedLines = [Line]()
-
-func isPossible() -> Bool {
-    var persons = [Int]()
-
-    for person in 0..<n {
-        persons.append(person)
-    }
-
-    for line in selectedLines {
-        var temp = persons[line.a]
-        persons[line.a] = persons[line.a + 1]
-        persons[line.a + 1] = temp
-    }
-
-    // print(persons)
-    // print(sortedSelectedLines)
-    if pivot == persons {
-        return true
-    }
-
-    return false
-}
-
+var map = initialMap(lines)
 var minCount = lines.count
+let pivot = ladderGame(map)
 
-func findCombination(_ cnt: Int) {
-    if cnt == m {
-        if isPossible() {
-            minCount = min(minCount, selectedLines.count)
-        }
-        return 
-    }
 
-    selectedLines.append(lines[cnt])
-    findCombination(cnt + 1)
-    selectedLines.removeLast()
+var chooseLines = [Line]()
+var visited = Set<[Line]>()
 
-    
-    findCombination(cnt + 1)
+func checkResult() {
+	guard visited.contains(chooseLines) == false
+	else { return }
+//	print(chooseLines)
+	let map = initialMap(chooseLines)
+	let result = ladderGame(map)
+	if pivot == result {
+		minCount = min(minCount, chooseLines.count)
+	}
+	visited.insert(chooseLines)
 }
 
+// a번째 세로줄과 a + 1번째 세로줄을 연결 => y
+// 위에서 부터 b번째 위치에 가로줄이 그어짐 => x
 
-
+func findCombination(_ count: Int) {
+	// n == 4
+	if count == m {
+		checkResult()
+		return
+	}
+	
+	chooseLines.append(lines[count])
+	findCombination(count + 1)
+	chooseLines.removeLast()
+	
+	findCombination(count + 1)
+	
+}
 
 findCombination(0)
-
 print(minCount)
