a5bf43a3d928d155349629ec8b308ae9fbd3b786
Lucite123 <164841152+Lucite123@users.noreply.github.com>
2024-04-27 20:21:23 +0900

    Title: 두 수의 합과 1, Time: 13ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240427/두 수의 합과 1/two-nums-sum-and-1.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+let numbers = readLine()!.trimmingCharacters(in: .whitespaces).components(separatedBy: " ").map { Int(String($0))! }
+
+var sum = numbers.reduce(0, +)
+
+var count = 0
+while sum > 10 {
+    let isOne = sum % 10
+
+    if isOne == 1 {
+        count += 1
+    }
+
+    sum /= 10
+}
+
+if sum == 1 {
+    count += 1
+}
+
+print(count)
