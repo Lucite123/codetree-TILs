02274b8cbe45784a010e9483348f757dc7dd54ba
Lucite123 <164841152+Lucite123@users.noreply.github.com>
2024-11-07 11:48:19 +0900

    Title: 배열의 평균, Time: 11ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 241107/배열의 평균/ave-of-array.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+let n = 2
+
+var numbers = [[Double]]()
+
+for _ in 0 ..< n {
+    let input = readLine()!.trimmingCharacters(in: .whitespaces).components(separatedBy: " ").map { Double(String($0))! }
+    numbers.append(input)
+}
+
+// 가로평균
+for number in numbers {
+    let sum = number.reduce(0, +)
+    let average: Double = sum / Double(number.count)
+    print(String(format: "%.1f", average), terminator: " ")
+}
+
+print()
+/// 세로 평균
+
+for index in 0 ..< 4 {
+    let up = numbers[0][index]
+    let down = numbers[1][index]
+
+    let average = (up + down) / 2.0
+    print(String(format: "%.1f", average), terminator: " ")
+}
+
+/// 전체 평균
+print()
+
+var total = 0.0
+
+for number in numbers {
+    total += number.reduce(0, + )
+}
+
+print(String(format: "%.1f",Double(total / 8)))
