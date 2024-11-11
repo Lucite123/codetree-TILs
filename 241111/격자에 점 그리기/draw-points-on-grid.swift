8ac37c74720534694aa06c1257c0cf09a1cf4310
Lucite123 <164841152+Lucite123@users.noreply.github.com>
2024-11-11 12:22:22 +0900

    Title: 격자에 점 그리기, Time: 11ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 241111/격자에 점 그리기/draw-points-on-grid.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+let nm = readLine()!.trimmingCharacters(in: .whitespaces).components(separatedBy: " ").map { Int(String($0))! }
+let n = nm[0]
+let m = nm[1]
+
+var map = Array(repeating: Array(repeating: 0, count: n), count: n)
+
+for k in 1 ... m {
+    let rc = readLine()!.trimmingCharacters(in: .whitespaces).components(separatedBy: " ").map { Int(String($0))! }
+    map[rc[0] - 1][rc[1] - 1] = k
+}
+
+for partialResult in map {
+    print(partialResult.map { String($0) }.joined(separator: " "))
+}
